b45dca3994c3c88fcef309070103f614af0ab389
Frank Sanchez <frank712c@gmail.com>
2016-12-10 19:35:33 +0100

    implemented the custom menu tab bar, and updated the settings for each icon in the assets folder. It was part of the custom tab bar process. I also updated the bijenkorf logo for the launch screen and navbar in the assets folder for better quality images.  Lastly, adjusted the constraints on bijenkorf logo on the launch screen.

---
+++ SwipingApp/CustomTabBarItem.swift
@@ -0,0 +1,44 @@
+//
+//  CustomTabBarItem.swift
+//  SwipingApp
+//
+//  Created by Frank Sanchez on 12/10/16.
+//
+//
+import Foundation
+import UIKit
+
+class CustomTabBarItem: UIView {
+    
+    var iconView: UIImageView!
+    
+    override init (frame : CGRect) {
+        super.init(frame : frame)
+        
+    }
+    
+    convenience init () {
+        self.init(frame:CGRect.zero)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup(_ item: UITabBarItem) {
+        
+        guard let image = item.image else {
+            fatalError("add images to tabbar items")
+        }
+        
+        // create imageView centered within a container
+        iconView = UIImageView(frame: CGRect(x: (self.frame.width-image.size.width)/2, y: (self.frame.height-image.size
+            .height)/2, width: self.frame.width, height: self.frame.height))
+        
+        iconView.image = image
+        iconView.sizeToFit()
+        iconView.tintColor = UIColor.lightGray
+        self.addSubview(iconView)
+    }
+    
+}
